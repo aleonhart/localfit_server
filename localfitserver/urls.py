39e3dd15890ac5193b4a7e8b1cbde6373dd89974
Alex Leonhart <alexleonhartcode@gmail.com>
2020-01-03 13:31:58 -0800

    adding activity file upload to available urls

--- localfitserver/urls.py
+++ localfitserver/urls.py
@@ -7,5 +7,6 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('monitor/', include(('monitor.urls', 'monitor'), namespace='monitor')),
+    path('activity/', include(('activity.urls', 'activity'), namespace='activity')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
