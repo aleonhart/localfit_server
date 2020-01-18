dec1be7cdb905e9a6817fe8b91c6eac798989912
Alex Leonhart <alexleonhartcode@gmail.com>
2020-01-17 16:53:51 -0800

    new model for walk session data

--- activity/models.py
+++ activity/models.py
@@ -6,7 +6,31 @@ class ActivityWalkFile(models.Model):
     Activity Walk files (ACTIVITY)
     """
     filename = models.CharField(max_length=15, unique=True)
+    activity_type = models.CharField(default="walk", max_length=20)
     display_name = models.CharField(max_length=15, unique=True)
+
+
+class ActivityWalkSession(models.Model):
+    file = models.ForeignKey(ActivityWalkFile, related_name='activitywalksession', on_delete=models.CASCADE)
+    timestamp_utc = models.DateTimeField()
+    start_time_utc = models.DateTimeField()
+    start_position_lat_sem = models.IntegerField(null=True)
+    start_position_long_sem = models.IntegerField(null=True)
+    start_position_lat_deg = models.DecimalField(null=True, max_digits=17, decimal_places=14)
+    start_position_long_deg = models.DecimalField(null=True, max_digits=17, decimal_places=14)
+    total_elapsed_time = models.DecimalField(null=True, max_digits=7, decimal_places=3)
+    total_timer_time = models.DecimalField(null=True, max_digits=7, decimal_places=3)
+    total_distance = models.DecimalField(max_digits=8, decimal_places=2)
+    total_strides = models.IntegerField(null=True)
+    total_calories = models.IntegerField(null=True)
+    enhanced_avg_speed = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    avg_speed = models.IntegerField(null=True)
+    enhanced_max_speed = models.DecimalField(max_digits=5, decimal_places=3, null=True)
+    max_speed = models.IntegerField(null=True)
+    avg_power = models.IntegerField(null=True)
+    max_power = models.IntegerField(null=True)
+    total_ascent = models.IntegerField(null=True)
+    total_descent = models.IntegerField(null=True)
 
 
 class ActivityWalkData(models.Model):
@@ -47,7 +71,7 @@ class ActivityWalkData(models.Model):
     position_long_deg = models.DecimalField(null=True, max_digits=9, decimal_places=6)   #     XXX.XXXXXX degrees
     distance = models.DecimalField(max_digits=8, decimal_places=2)                       # XXX,XXX.XX  meters, 100mi is 160,934m
     altitude = models.DecimalField(max_digits=5, decimal_places=1)                       #   X,XXX.X   meters, Mt. Everest is 8,850m high
-    speed = models.DecimalField(max_digits=5, decimal_places=3)                          #      XX.XXX meters/second, Usain Bolt's top speed is 12.27m/s
+    speed = models.IntegerField()                                                        #      XX.XXX meters/second, Usain Bolt's top speed is 12.27m/s
     heart_rate = models.IntegerField()                                                   # BPM
     cadence = models.IntegerField()                                                      # RPM
     fractional_cadence = models.DecimalField(max_digits=5, decimal_places=1)             # RPM
