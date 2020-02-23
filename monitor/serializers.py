4adca0649a8edddfb55b97cac8c670ee63e84682
Alex Leonhart <alexleonhartcode@gmail.com>
2020-02-23 10:59:30 -0800

    mocking out new stub api for stress range data

--- monitor/serializers.py
+++ monitor/serializers.py
@@ -1,5 +1,7 @@
 # 3rd Party
+from django.utils import timezone
 from rest_framework import serializers
+from rest_framework.utils.serializer_helpers import ReturnDict
 
 # Internal
 from .models import HeartRateData, RestingMetRateData
@@ -39,3 +41,78 @@ class StressDataSerializer(serializers.Serializer):
 
     class Meta:
         list_serializer_class = StressDataListSerializer
+
+
+class PieChartListSerializer(serializers.ListSerializer):
+    """
+    Range: 0 to 100,
+    0 to 25: REST
+    26 to 50: LOW
+    51 to 75: MED
+    76 to 100: HIGH
+    """
+
+    def _format_for_pie_chart(self, data):
+        return [
+            {
+                "date": "2020-01-01",
+                "stress_data": {
+                    "REST": 10,
+                    "LOW": 20,
+                    "MED": 30,
+                    "HIGH": 40
+                }
+            },
+            {
+                "date": "2020-01-02",
+                "stress_data": {
+                    "REST": 10,
+                    "LOW": 20,
+                    "MED": 30,
+                    "HIGH": 40
+                }
+            },
+            {
+                "date": "2020-01-03",
+                "stress_data": {
+                    "REST": 10,
+                    "LOW": 20,
+                    "MED": 30,
+                    "HIGH": 40
+                }
+            },
+            {
+                "date": "2020-01-04",
+                "stress_data": {
+                    "REST": 10,
+                    "LOW": 20,
+                    "MED": 30,
+                    "HIGH": 40
+                }
+            },
+            {
+                "date": "2020-01-05",
+                "stress_data": {
+                    "REST": 10,
+                    "LOW": 20,
+                    "MED": 30,
+                    "HIGH": 40
+                }
+            },
+        ]
+
+    @property
+    def data(self):
+        data = self._format_for_pie_chart(self.instance)
+        response = {
+            # 'start_time': timezone.localtime(data[0]['t']) if data else [],
+            # 'end_time': timezone.localtime(data[-1]['t']) if data else [],
+            'stress': data
+        }
+        return ReturnDict(response, serializer=self)
+
+
+class PieChartSerializer(serializers.Serializer):
+
+    class Meta:
+        list_serializer_class = PieChartListSerializer
--- monitor/urls.py
+++ monitor/urls.py
@@ -3,12 +3,13 @@ from django.urls import include, path
 from rest_framework import routers
 
 # Internal
-from .views import MonitorFileUpload, StressList, HeartRateList, RestingMetaList
+from .views import MonitorFileUpload, StressList, HeartRateList, RestingMetaList, StressRange
 
 
 router = routers.DefaultRouter()
 router.register(r'upload', MonitorFileUpload)
 router.register(r'stress_data', StressList)
+router.register(r'stress_range', StressRange)
 router.register(r'heart_rate', HeartRateList)
 router.register(r'resting_meta', RestingMetaList)
 
