dfa80dab69a953c6e043467b0a13b07c98668f82
Alex Leonhart <alexleonhartcode@gmail.com>
2020-02-23 11:57:24 -0800

    get real data for stress range charts

--- monitor/serializers.py
+++ monitor/serializers.py
@@ -1,4 +1,9 @@
+# stdlib
+from datetime import datetime, time
+from decimal import Decimal
+
 # 3rd Party
+from django.db.models.functions.datetime import TruncDate
 from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.utils.serializer_helpers import ReturnDict
@@ -44,70 +49,62 @@ class StressDataSerializer(serializers.Serializer):
 
 
 class PieChartListSerializer(serializers.ListSerializer):
-    """
-    Range: 0 to 100,
-    0 to 25: REST
-    26 to 50: LOW
-    51 to 75: MED
-    76 to 100: HIGH
-    """
+
+    def _get_range_by_value(self, value):
+        range = "NONE"
+        if 0 <= value <= 25:
+            range = "REST"
+        if 25 < value <= 50:
+            range = "LOW"
+        if 50 < value <= 75:
+            range = "MED"
+        if 75 < value <= 100:
+            range = "HIGH"
+
+        return range
+
+    def _build_stress_payload(self, data, date_range):
+        stress_ranges = [
+            self._get_range_by_value(d.stress_level_value) for d in
+            data.filter(**date_range)
+        ]
+        rest = stress_ranges.count("REST")
+        low = stress_ranges.count("LOW")
+        med = stress_ranges.count("MED")
+        high = stress_ranges.count("HIGH")
+        actual_stress_data_range = rest + low + med + high
+        return {
+            "date": date_range['stress_level_time_utc__range'][0].date(),
+            "stress_data": {
+                "REST": round(Decimal((rest / actual_stress_data_range) * 100), 0),
+                "LOW": round(Decimal((low / actual_stress_data_range) * 100), 0),
+                "MED": round(Decimal((med / actual_stress_data_range) * 100), 0),
+                "HIGH": round(Decimal((high / actual_stress_data_range) * 100), 0)
+            }
+        }
 
     def _format_for_pie_chart(self, data):
-        return [
-            {
-                "date": "2020-01-01",
-                "stress_data": {
-                    "REST": 10,
-                    "LOW": 20,
-                    "MED": 30,
-                    "HIGH": 40
-                }
-            },
-            {
-                "date": "2020-01-02",
-                "stress_data": {
-                    "REST": 10,
-                    "LOW": 20,
-                    "MED": 30,
-                    "HIGH": 40
-                }
-            },
-            {
-                "date": "2020-01-03",
-                "stress_data": {
-                    "REST": 10,
-                    "LOW": 20,
-                    "MED": 30,
-                    "HIGH": 40
-                }
-            },
-            {
-                "date": "2020-01-04",
-                "stress_data": {
-                    "REST": 10,
-                    "LOW": 20,
-                    "MED": 30,
-                    "HIGH": 40
-                }
-            },
-            {
-                "date": "2020-01-05",
-                "stress_data": {
-                    "REST": 10,
-                    "LOW": 20,
-                    "MED": 30,
-                    "HIGH": 40
-                }
-            },
+        stress_response = []
+        stress_dates = [
+            d['date'] for d in
+            data.annotate(date=TruncDate('stress_level_time_utc')).values('date').distinct()
         ]
+        for stress_date in stress_dates:
+            date_range = {
+                'stress_level_time_utc__range': (
+                datetime.combine(stress_date, time.min), datetime.combine(stress_date, time.max))
+            }
+            stress_payload = self._build_stress_payload(data, date_range)
+            stress_response.append(stress_payload)
+        return stress_dates, stress_response
 
     @property
     def data(self):
-        data = self._format_for_pie_chart(self.instance)
+        stress_dates, stress_response = self._format_for_pie_chart(self.instance)
         response = {
-            # 'start_time': timezone.localtime(data[0]['t']) if data else [],
-            # 'end_time': timezone.localtime(data[-1]['t']) if data else [],
-            'stress': data
+            'start_date': stress_dates[0],
+            'end_date': stress_dates[-1],
+            'stress': stress_response
         }
         return ReturnDict(response, serializer=self)
 
